e9e2c2e8d83588a2af92b021c6ceed17d70e5bc8
Nermien19 <116633366+Nermien19@users.noreply.github.com>
2024-03-12 06:15:09 +0200

    doctor's repository again

--- demo/src/main/java/com/example/demo/repositories/doctorRepository.java
+++ demo/src/main/java/com/example/demo/repositories/doctorRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.models.doctor;
 
 @Repository
-public interface doctorRepository extends JpaRepository<doctor,Integer>{
 
-    
+public interface doctorRepository extends JpaRepository<doctor, Integer> {
 }
