6d986b25680ee53630fea5f2cf99a1131d015561
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2024-03-12 23:51:47 +0200

    user model

--- demo/src/main/java/com/example/demo/models/User.java
+++ demo/src/main/java/com/example/demo/models/User.java
@@ -9,7 +9,33 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int Id;
+    private String name;
     private String email;
     private String phone;
     private String password;
+
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+    
+    public String getUsername() {
+        return this.name;
+    }
+
+    public void setUsername(String name) {
+        this.name = name;
+    }
+    
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
