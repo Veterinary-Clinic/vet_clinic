5aa7349c411542bc989f3657e0ed9a076274c4cb
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2024-03-15 01:20:56 +0200

    sign up

--- demo/src/main/java/com/example/demo/models/User.java
+++ demo/src/main/java/com/example/demo/models/User.java
@@ -1,14 +1,16 @@
 package com.example.demo.models;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-@Entity
+import java.util.Objects;
 
+@Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int Id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
     private String name;
     private String email;
     private String phone;
@@ -17,25 +19,104 @@ public class User {
     public User() {
     }
 
-    public User(String name, String password) {
+    public User(Long Id, String name, String email, String phone, String password) {
+        this.Id = Id;
         this.name = name;
+        this.email = email;
+        this.phone = phone;
         this.password = password;
     }
-    
-    public String getUsername() {
+
+    public Long getId() {
+        return this.Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
         return this.name;
     }
 
-    public void setUsername(String name) {
+    public void setName(String name) {
         this.name = name;
     }
-    
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     public String getPassword() {
         return this.password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public User Id(Long Id) {
+        setId(Id);
+        return this;
+    }
+
+    public User name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public User email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    public User phone(String phone) {
+        setPhone(phone);
+        return this;
+    }
+
+    public User password(String password) {
+        setPassword(password);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(Id, user.Id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(phone, user.phone) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, name, email, phone, password);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " Id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", email='" + getEmail() + "'" +
+            ", phone='" + getPhone() + "'" +
+            ", password='" + getPassword() + "'" +
+            "}";
     }
 
 }
