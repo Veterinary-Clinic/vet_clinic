50c34556429ab6bb569386298657003be7a9ec8c
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2024-03-15 20:48:34 +0200

    logout

--- demo/src/main/java/com/example/demo/controllers/DoctorController.java
+++ demo/src/main/java/com/example/demo/controllers/DoctorController.java
@@ -131,4 +131,11 @@ public class DoctorController {
         doctorRepository.delete(doctor);
         return "redirect:/admin/doctors";
     }
+
+    @GetMapping("/logout")
+    public RedirectView logout(HttpSession session) {
+        // Invalidate the session
+        session.invalidate();
+        return new RedirectView("/doctor/login");
+}
 }
