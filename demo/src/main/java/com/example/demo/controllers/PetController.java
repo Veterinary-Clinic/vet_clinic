3c03ef972725f5e61e0dd5a1aafec950a15e2ef3
nadaelamir101 <125657577+nadaelamir101@users.noreply.github.com>
2024-03-16 00:03:37 +0200

    redirect page

--- demo/src/main/java/com/example/demo/controllers/PetController.java
+++ demo/src/main/java/com/example/demo/controllers/PetController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,7 +8,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+
 import java.util.List;
+
+import com.example.demo.models.Doctor;
 import com.example.demo.models.Pet;
 import com.example.demo.repositories.PetRepository;
 
@@ -34,8 +40,8 @@ public class PetController {
     }
     
     @PostMapping("addPet")
-    public String savePet(@ModelAttribute Pet npet) {
-        this.petRepository.save(npet);
-        return "added";
+    public RedirectView savePet(@ModelAttribute Pet npet) {
+        petRepository.save(npet);
+        return new RedirectView("pets");
     }
 }
