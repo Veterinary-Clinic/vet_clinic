2b8de93cc083aca1ba3447fa14f8d3dbb8f96444
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2024-03-16 13:52:01 +0200

    login

--- demo/src/main/java/com/example/demo/repositories/UserRepository.java
+++ demo/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -10,5 +10,6 @@ import com.example.demo.models.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long Id); 
+    User findByEmail(String email);
 }
 
