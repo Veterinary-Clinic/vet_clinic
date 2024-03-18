b99550d937d3b1213951fa6b20a8d3fab62d267b
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2024-03-18 04:03:07 +0200

    Update UserRepository.java

--- demo/src/main/java/com/example/demo/repositories/UserRepository.java
+++ demo/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -12,5 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long Id); 
     User findByemail(String email);
     Object findByEmail(String email);
+     User save(User user);
 }
 
